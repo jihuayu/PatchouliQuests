4f816871f2a11b6804dc1ab7a5147cbb92097020
jihuayu <8042833@qq.com>
2020-05-18 10:36:28 +0800

    up

--- src/main/resources/coremod/BookRegistry.js
+++ src/main/resources/coremod/BookRegistry.js
@@ -9,14 +9,11 @@ function initializeCoreMod() {
                 'name': 'vazkii/patchouli/common/book/BookRegistry'
             },
             'transformer': function (cn) {
-                //遍历ClassNode下的methods
                 cn.fields.forEach(function (mn) {
                     if (mn.name === 'GSON') {
                         mn.access -= Opcodes.ACC_FINAL;
                     }
                 });
-
-                //返回修改后的ClassNode对象
                 return cn;
             }
         }
